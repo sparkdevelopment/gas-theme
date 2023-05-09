0bcf45f571675cc1810a27ab45885424cee4d8ea
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-05-09 01:22:35 +0100

    Remove additional classes on custom widget area

--- inc/custom-widget-areas.php
+++ inc/custom-widget-areas.php
@@ -7,7 +7,7 @@ function gas_footer_newsletter_widget_area() {
             'name'          => __( 'Footer Newsletter', 'gas-production-hire' ),
             'id'            => 'footer-newsletter',
             'description'   => __( 'Add a newsletter form to the footer.', 'gas-production-hire' ),
-            'before_widget' => '<div class="sm:my-6 my-2 pt-8 hidden sm:block text-right">',
+            'before_widget' => '<div>',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="text-white text-xl font-light leading-6">',
             'after_title'   => '</h3>',
