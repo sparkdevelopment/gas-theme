8385332519abff63215dea58d60539fbc28205e8
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-18 00:40:54 +0100

    Hide additional image buttons if no additional images

--- template-parts/product-single.php
+++ template-parts/product-single.php
@@ -3,6 +3,9 @@
 // Template for the product single page
 // Path: template-parts/product-single.php
 
+// Get the additional product images
+$images = get_post_meta( get_the_ID(), 'product_images', true );
+
 ?>
 
 <article id="product-<?php the_ID(); ?>" <?php post_class( 'bg-black w-full text-white' ); ?>>
@@ -14,13 +17,12 @@
 				<div><?php display_breadcrumbs(); ?></div>
 			</div>
 			<div class="relative my-16 flex items-center">
+			<?php if ( $images ): ?>
 				<img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" alt="Arrow pointing left"  class="js-carousel-previous absolute left-14 bottom-0 w-[1.5vw] mx-auto mt-[2vh] mb-[1vh] rotate-90 mr-[1.5vw] cursor-pointer z-10 invert mix-blend-difference">
+				<?php endif; ?>
 				<div class="owl-carousel w-full aspect-square">
 					<div class="background-image w-full bg-cover bg-center grow aspect-square" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_ID(), 'product' ); ?>');"></div>
 					<?php
-					// Get the rest of the images
-					$images = get_post_meta( get_the_ID(), 'product_images', true );
-
 					// Loop through the images
 					foreach ( $images as $image ) :
 
@@ -34,7 +36,9 @@
 					endforeach;
 					?>
 				</div>
+				<?php if ( $images ): ?>
 				<img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" alt="Arrow pointing right" class="js-carousel-next absolute right-14 bottom-0 w-[1.5vw] mx-auto mt-[2vh] mb-[1vh] -rotate-90 ml-[1.5vw] cursor-pointer z-10 invert mix-blend-difference">
+				<?php endif; ?>
 			</div>
 			<div class="text-2xl font-light text-left space-y-5 mx-8 my-4">
 				<?php
