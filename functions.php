95563b82ac30513405300aa79f68382ef72bcb26
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-17 23:23:46 +0100

    Fix breadcrumbs

--- functions.php
+++ functions.php
@@ -3,13 +3,12 @@
 /**
  * Theme setup.
  */
-function tailpress_setup()
-{
-	add_theme_support('title-tag');
+function tailpress_setup() {
+	add_theme_support( 'title-tag' );
 
 	register_nav_menus(
 		array(
-			'primary' => __('Primary Menu', 'tailpress'),
+			'primary' => __( 'Primary Menu', 'tailpress' ),
 		)
 	);
 
@@ -24,55 +23,55 @@ function tailpress_setup()
 		)
 	);
 
-	add_theme_support('custom-logo');
-	add_theme_support('post-thumbnails');
+	add_theme_support( 'custom-logo' );
+	add_theme_support( 'post-thumbnails' );
 
-	add_theme_support('align-wide');
-	add_theme_support('wp-block-styles');
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'wp-block-styles' );
 
-	add_theme_support('editor-styles');
-	add_theme_support('widgets');
-	add_editor_style('css/editor-style.css');
+	add_theme_support( 'editor-styles' );
+	add_theme_support( 'widgets' );
+	add_editor_style( 'css/editor-style.css' );
 }
 
-add_action('after_setup_theme', 'tailpress_setup');
+add_action( 'after_setup_theme', 'tailpress_setup' );
 
 /**
  * Enqueue theme assets.
  */
 function tailpress_enqueue_scripts() {
 	$theme = wp_get_theme();
-	
-    // Enqueue styles
-	wp_enqueue_style('tailpress', tailpress_asset('css/app.css'), array(), $theme->get('Version'));
-    wp_enqueue_style('owl-carousel-css', tailpress_asset('css/owl.carousel.min.css'), array(), $theme->get('Version'));
-    wp_enqueue_style('owl-carousel-theme', tailpress_asset('css/owl.theme.default.min.css'), array(), $theme->get('Version'));
+
+	// Enqueue styles
+	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'owl-carousel-css', tailpress_asset( 'css/owl.carousel.min.css' ), array(), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'owl-carousel-theme', tailpress_asset( 'css/owl.theme.default.min.css' ), array(), $theme->get( 'Version' ) );
 	if ( is_front_page() ) {
-		wp_enqueue_style('docslider', tailpress_asset('css/docSlider.css'), array('tailpress'), $theme->get('Version'));
+		wp_enqueue_style( 'docslider', tailpress_asset( 'css/docSlider.css' ), array( 'tailpress' ), $theme->get( 'Version' ) );
 	}
 
-    // Enqueue scripts
+	// Enqueue scripts
 	if ( is_front_page() || is_singular( 'page' ) ) {
-		wp_enqueue_script('lethargy', tailpress_asset('js/lethargy.min.js'), array(), $theme->get('Version'), true);
-    	wp_enqueue_script('docslider', tailpress_asset('js/docSlider.js'), array('lethargy'), $theme->get('Version'), true);
-		wp_enqueue_script('owl-carousel', tailpress_asset('js/owl.carousel.min.js'), array('jquery'), $theme->get('Version'), true);
-		wp_enqueue_script('gas-homepage', tailpress_asset('js/homepage.js'), array('jquery','lethargy','docslider','owl-carousel'), $theme->get('Version'), true);
+		wp_enqueue_script( 'lethargy', tailpress_asset( 'js/lethargy.min.js' ), array(), $theme->get( 'Version' ), true );
+		wp_enqueue_script( 'docslider', tailpress_asset( 'js/docSlider.js' ), array( 'lethargy' ), $theme->get( 'Version' ), true );
+		wp_enqueue_script( 'owl-carousel', tailpress_asset( 'js/owl.carousel.min.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
+		wp_enqueue_script( 'gas-homepage', tailpress_asset( 'js/homepage.js' ), array( 'jquery', 'lethargy', 'docslider', 'owl-carousel' ), $theme->get( 'Version' ), true );
 	}
-	wp_enqueue_script('gas', tailpress_asset('js/app.js'), array('jquery'), $theme->get('Version'), true);
+	wp_enqueue_script( 'gas', tailpress_asset( 'js/app.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
 
 	if ( is_singular( 'product' ) ) {
-		wp_enqueue_script('owl-carousel', tailpress_asset('js/owl.carousel.min.js'), array('jquery'), $theme->get('Version'), true);
-		wp_enqueue_script('gas-product-single', tailpress_asset('js/productSingle.js'), array('jquery'), $theme->get('Version'), true);
+		wp_enqueue_script( 'owl-carousel', tailpress_asset( 'js/owl.carousel.min.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
+		wp_enqueue_script( 'gas-product-single', tailpress_asset( 'js/productSingle.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
 	}
 
 	// Load search on all pages
-	wp_enqueue_script('gas-search', tailpress_asset('js/search.js'), array('jquery'), $theme->get('Version'), true);
+	wp_enqueue_script( 'gas-search', tailpress_asset( 'js/search.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
 
-    // Enqueue jQuery from Google CDN
-    wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), null, true);
+	// Enqueue jQuery from Google CDN
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), null, true );
 }
-add_action('wp_enqueue_scripts', 'tailpress_enqueue_scripts');	
+add_action( 'wp_enqueue_scripts', 'tailpress_enqueue_scripts' );
 
 /**
  * Enqueue admin assets.
@@ -80,29 +79,28 @@ add_action('wp_enqueue_scripts', 'tailpress_enqueue_scripts');
 function tailpress_enqueue_admin_scripts() {
 	$theme = wp_get_theme();
 	wp_enqueue_media();
-	
+
 	// Enqueue styles
-	wp_enqueue_style('tailpress-admin', tailpress_asset('css/admin.css'), array(), $theme->get('Version'));
+	wp_enqueue_style( 'tailpress-admin', tailpress_asset( 'css/admin.css' ), array(), $theme->get( 'Version' ) );
 
 	// Enqueue scripts
-	wp_enqueue_script('tailpress-admin', tailpress_asset('js/admin.js'), array('jquery'), $theme->get('Version'), true);
+	wp_enqueue_script( 'tailpress-admin', tailpress_asset( 'js/admin.js' ), array( 'jquery' ), $theme->get( 'Version' ), true );
 }
-add_action('admin_enqueue_scripts', 'tailpress_enqueue_admin_scripts');
+add_action( 'admin_enqueue_scripts', 'tailpress_enqueue_admin_scripts' );
 
 /**
  * Get asset path.
  *
- * @param string  $path Path to asset.
+ * @param string $path Path to asset.
  *
  * @return string
  */
-function tailpress_asset($path)
-{
-	if (wp_get_environment_type() === 'production') {
+function tailpress_asset( $path ) {
+	if ( wp_get_environment_type() === 'production' ) {
 		return get_stylesheet_directory_uri() . '/' . $path;
 	}
 
-	return add_query_arg('time', time(),  get_stylesheet_directory_uri() . '/' . $path);
+	return add_query_arg( 'time', time(), get_stylesheet_directory_uri() . '/' . $path );
 }
 
 /**
@@ -114,20 +112,19 @@ function tailpress_asset($path)
  *
  * @return array
  */
-function tailpress_nav_menu_add_li_class($classes, $item, $args, $depth)
-{
-	if (isset($args->li_class)) {
+function tailpress_nav_menu_add_li_class( $classes, $item, $args, $depth ) {
+	if ( isset( $args->li_class ) ) {
 		$classes[] = $args->li_class;
 	}
 
-	if (isset($args->{"li_class_$depth"})) {
+	if ( isset( $args->{"li_class_$depth"} ) ) {
 		$classes[] = $args->{"li_class_$depth"};
 	}
 
 	return $classes;
 }
 
-add_filter('nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4);
+add_filter( 'nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4 );
 
 /**
  * Adds option 'submenu_class' to 'wp_nav_menu'.
@@ -138,20 +135,19 @@ add_filter('nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4);
  *
  * @return array
  */
-function tailpress_nav_menu_add_submenu_class($classes, $args, $depth)
-{
-	if (isset($args->submenu_class)) {
+function tailpress_nav_menu_add_submenu_class( $classes, $args, $depth ) {
+	if ( isset( $args->submenu_class ) ) {
 		$classes[] = $args->submenu_class;
 	}
 
-	if (isset($args->{"submenu_class_$depth"})) {
+	if ( isset( $args->{"submenu_class_$depth"} ) ) {
 		$classes[] = $args->{"submenu_class_$depth"};
 	}
 
 	return $classes;
 }
 
-add_filter('nav_menu_submenu_css_class', 'tailpress_nav_menu_add_submenu_class', 10, 3);
+add_filter( 'nav_menu_submenu_css_class', 'tailpress_nav_menu_add_submenu_class', 10, 3 );
 
 // Include custom post types
 require_once get_template_directory() . '/inc/product-post-type.php';
@@ -162,57 +158,49 @@ require_once get_template_directory() . '/inc/custom-widget-areas.php';
 // Build breadcrumbs for single product and product category pages showing parent and child categories, iterating over all levels
 function build_breadcrumbs() {
 	$breadcrumbs = array();
-	$breadcrumbs[] = array(
-		'title' => 'Home',
-		'link' => get_home_url(),
-	);
+
 	if ( is_product() ) {
-		$categories = get_the_terms( get_the_ID(), 'product_category' );
-		if ( $categories ) {
-			foreach ( $categories as $category ) {
-				// If the category has a parent, add the parent to the breadcrumbs
-				if ( $category->parent ) {
-					$parent = get_term( $category->parent, 'product_category' );
-					$breadcrumbs[] = array(
-						'title' => $parent->name,
-						'link' => get_term_link( $parent ),
-					);
-				}
-				$breadcrumbs[] = array(
-					'title' => $category->name,
-					'link' => get_term_link( $category ),
-				);
-			}
-		}
 		$breadcrumbs[] = array(
 			'title' => get_the_title(),
-			'link' => get_the_permalink(),
+			'link'  => get_the_permalink(),
 		);
-	} elseif ( is_product_category() ) {
-		$categories = get_the_terms( get_the_ID(), 'product_category' );
-		if ( $categories ) {
-			foreach ( $categories as $category ) {
-				// If the category has a parent, add the parent to the breadcrumbs
-				if ( $category->parent ) {
-					$parent = get_term( $category->parent, 'product_category' );
-					$breadcrumbs[] = array(
-						'title' => $parent->name,
-						'link' => get_term_link( $parent ),
-					);
-				}
-				$breadcrumbs[] = array(
-					'title' => $category->name,
-					'link' => get_term_link( $category ),
-				);
+	}
+
+	$categories = get_the_terms( get_the_ID(), 'product_category' );
+	if ( ! empty( $categories ) ) {
+		$current_category = $categories[0];
+
+		while ( $current_category ) {
+			array_unshift(
+				$breadcrumbs,
+				array(
+					'title' => $current_category->name,
+					'link'  => get_term_link( $current_category ),
+				)
+			);
+
+			if ( $current_category->parent ) {
+				$current_category = get_term( $current_category->parent, 'product_category' );
+			} else {
+				break;
 			}
 		}
 	}
+
+	array_unshift(
+		$breadcrumbs,
+		array(
+			'title' => 'Home',
+			'link'  => get_home_url(),
+		)
+	);
+
 	return $breadcrumbs;
 }
 
 // Display breadcrumbs separated by a forward slash
 function display_breadcrumbs() {
-	$breadcrumbs = build_breadcrumbs();
+	$breadcrumbs      = build_breadcrumbs();
 	$breadcrumb_count = count( $breadcrumbs );
 	$breadcrumb_index = 1;
 	foreach ( $breadcrumbs as $breadcrumb ) {
@@ -225,7 +213,7 @@ function display_breadcrumbs() {
 		if ( $breadcrumb_index < $breadcrumb_count ) {
 			echo ' / ';
 		}
-		$breadcrumb_index++;
+		++$breadcrumb_index;
 	}
 }
 
@@ -245,7 +233,7 @@ add_filter( 'wpcf7_autop_or_not', '__return_false' );
 
 // Add logo classes
 function adjust_logo_classes( $html ) {
-	$html = str_replace('custom-logo-link', 'block [&>img]:mx-8 [&>img]:md:mx-0 [&>img]:my-7 [&>img]:md:my-0 [&>img]:h-10 [&>img]:md:h-auto [&>img]:w-auto', $html );
+	$html = str_replace( 'custom-logo-link', 'block [&>img]:mx-8 [&>img]:md:mx-0 [&>img]:my-7 [&>img]:md:my-0 [&>img]:h-10 [&>img]:md:h-auto [&>img]:w-auto', $html );
 	return $html;
 }
-add_filter('get_custom_logo', 'adjust_logo_classes', 10);
+add_filter( 'get_custom_logo', 'adjust_logo_classes', 10 );
