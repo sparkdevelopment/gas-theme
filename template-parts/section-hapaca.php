03361f6246e273588171e575d57e1a413be2f27b
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-20 14:42:24 +0100

    Fix missing Hapaca logo

--- template-parts/section-hapaca.php
+++ template-parts/section-hapaca.php
@@ -1,7 +1,7 @@
 <section id="section-hapaca" class="h-screen bg-black flex lg:flex-row flex-col-reverse text-white max-w-full">
     <div class="lg:w-[25vw] w-full lg:h-full h-[34vh] font-theme-heading lg:text-left text-center flex">
         <div class="m-auto text-left">
-            <a href="https://hapacastudio.com/" target="_blank"><img src="<?php echo get_theme_file_uri(); ?>/resources/img/hapaca.svg" class="fade-in block invert max-h-[15vh] -ml-[6vw] lg:-ml-[2vw] -mt-[7vh] lg:mt-0"></a>
+            <a href="https://hapacastudio.com/" target="_blank"><img src="<?php echo get_theme_file_uri(); ?>/resources/img/hapaca.svg" class="fade-in block invert h-[15vh] -ml-[6vw] lg:-ml-[2vw] -mt-[7vh] lg:mt-0"></a>
             <p class="fade-in block text-[3.9vmin] lg:text-lg xl:text-xl 2xl:text-2xl font-light lg:py-2 xl:py-3 px-4">GAS are proud to be the sole supplier of Hapaca - unique, hand painted canvas backdrops and various types of fabrics for photographic and film sets, interior designers, fashion shows, exhibitions and more.</p>
         </div>
     </div>
