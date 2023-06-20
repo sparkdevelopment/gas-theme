0d2e7d88ad569dd431f5d3da85bc62eeeca57bc0
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-20 16:12:20 +0100

    Add arrow back to Hapaca page

--- template-parts/section-hapaca.php
+++ template-parts/section-hapaca.php
@@ -3,6 +3,7 @@
         <div class="m-auto text-left">
             <a href="https://hapacastudio.com/" target="_blank"><img src="<?php echo get_theme_file_uri(); ?>/resources/img/hapaca.svg" class="fade-in block invert h-[15vh] -ml-[6vw] lg:-ml-[2vw] -mt-[7vh] lg:mt-0"></a>
             <p class="fade-in block text-[3.9vmin] lg:text-lg xl:text-xl 2xl:text-2xl font-light lg:py-2 xl:py-3 px-4">GAS are proud to be the sole supplier of Hapaca - unique, hand painted canvas backdrops and various types of fabrics for photographic and film sets, interior designers, fashion shows, exhibitions and more.</p>
+            <a class="fade-in block text-[4vmin] lg:text-lg xl:text-xl 2xl:text-2xl font-normal px-4 my-2 lg:my-0" href="https://hapacastudio.com/" target="_blank">Find out more <img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-white.svg" class="-rotate-90 w-2 lg:w-5 inline ml-4"></a>
         </div>
     </div>
     <div class="lg:w-[75vw] w-full lg:h-full h-[65vh] overflow-hidden">
