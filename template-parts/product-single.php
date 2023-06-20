48b544330ce5527be791af0f7097a04854b29c73
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-20 09:28:44 +0100

    Update single product template

--- template-parts/product-single.php
+++ template-parts/product-single.php
@@ -9,15 +9,28 @@ $images = get_post_meta( get_the_ID(), 'product_images', true );
 ?>
 
 <article id="product-<?php the_ID(); ?>" <?php post_class( 'bg-black w-full text-white' ); ?>>
-	<div class="pt-52 w-full !max-w-full">
-		<div class="js-product-details">
-			<div class="mx-8">
-				<h1 class="text-2xl lg:text-5xl font-light text-left font-theme-heading my-4"><?php the_title(); ?></h1>
-				<div class="text-2xl lg:text-4xl font-bold text-left my-4">&pound;<?php echo get_post_meta( get_the_ID(), 'product_price', true ); ?></div>
-				<div><?php display_breadcrumbs(); ?></div>
+	<div class="pt-52 pb-16 w-full !max-w-full flex">
+		<div class="js-product-details w-1/2">
+			<div class="mx-8 flex justify-between flex-col h-full">
+				<div>
+					<div><?php display_breadcrumbs(); ?></div>
+					<h1 class="text-2xl lg:text-5xl font-light text-left font-theme-heading my-4"><?php the_title(); ?></h1>
+					<div class="text-2xl lg:text-4xl font-bold text-left my-4">&pound;<?php echo get_post_meta( get_the_ID(), 'product_price', true ); ?></div>
+					<div class="text-2xl font-light text-left space-y-5 mx-8 my-4">
+						<?php
+						// Output description
+						echo wpautop( get_post_meta( get_the_ID(), 'product_description', true ) );
+						?>
+					</div>
+				</div>
+				<div class="text-sm text-center mt-16">
+					<p>Prices exclude VAT and insurance. Prices are subject to change, please contact a team member at GAS for further information</p>
+				</div>
 			</div>
-			<div class="relative my-16 flex items-center">
-			<?php if ( $images ): ?>
+		</div>
+		<div class="w-1/2">
+			<div class="relative flex">
+			<?php if ( $images ) : ?>
 				<img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" alt="Arrow pointing left"  class="js-carousel-previous absolute left-14 bottom-0 w-[1.5vw] mx-auto mt-[2vh] mb-[1vh] rotate-90 mr-[1.5vw] cursor-pointer z-10 invert mix-blend-difference">
 				<?php endif; ?>
 				<div class="owl-carousel w-full aspect-square">
@@ -36,18 +49,9 @@ $images = get_post_meta( get_the_ID(), 'product_images', true );
 					endforeach;
 					?>
 				</div>
-				<?php if ( $images ): ?>
+				<?php if ( $images ) : ?>
 				<img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" alt="Arrow pointing right" class="js-carousel-next absolute right-14 bottom-0 w-[1.5vw] mx-auto mt-[2vh] mb-[1vh] -rotate-90 ml-[1.5vw] cursor-pointer z-10 invert mix-blend-difference">
 				<?php endif; ?>
-			</div>
-			<div class="text-2xl font-light text-left space-y-5 mx-8 my-4">
-				<?php
-				// Output description
-				echo wpautop( get_post_meta( get_the_ID(), 'product_description', true ) );
-				?>
-			</div>
-			<div class="text-sm text-center mt-16">
-				<p>Prices exclude VAT and insurance. Prices are subject to change, please contact a team member at GAS for further information</p>
 			</div>
 		</div>
 	</div>
