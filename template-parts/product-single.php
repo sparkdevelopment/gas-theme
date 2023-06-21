12484c60b8b0a486fefe7bb47429d46e81766e8a
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-21 08:20:18 +0100

    Mobile product page changes

--- template-parts/product-single.php
+++ template-parts/product-single.php
@@ -9,9 +9,9 @@ $images = get_post_meta( get_the_ID(), 'product_images', true );
 ?>
 
 <article id="product-<?php the_ID(); ?>" <?php post_class( 'bg-black w-full text-white' ); ?>>
-	<div class="pt-52 pb-16 w-full !max-w-full flex">
-		<div class="js-product-details w-1/2">
-			<div class="mx-8 flex justify-between flex-col h-full">
+	<div class="pt-28 md:pt-52 pb-16 w-full !max-w-full flex flex-col-reverse md:flex-row">
+		<div class="js-product-details w-full md:w-1/2">
+			<div class="mx-3 md:mx-8 flex justify-between flex-col h-full">
 				<div>
 					<div><?php display_breadcrumbs(); ?></div>
 					<h1 class="text-2xl lg:text-5xl font-light text-left font-theme-heading my-4"><?php the_title(); ?></h1>
@@ -28,25 +28,27 @@ $images = get_post_meta( get_the_ID(), 'product_images', true );
 				</div>
 			</div>
 		</div>
-		<div class="w-1/2">
+		<div class="w-full md:w-1/2 mb-8 md:mb-0">
 			<div class="relative flex">
-			<?php if ( $images ) : ?>
+				<?php if ( $images ) : ?>
 				<img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" alt="Arrow pointing left"  class="js-carousel-previous absolute left-14 bottom-0 w-[1.5vw] mx-auto mt-[2vh] mb-[1vh] rotate-90 mr-[1.5vw] cursor-pointer z-10 invert mix-blend-difference">
 				<?php endif; ?>
 				<div class="owl-carousel w-full aspect-square">
 					<div class="background-image w-full bg-cover bg-center grow aspect-square" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_ID(), 'product' ); ?>');"></div>
 					<?php
 					// Loop through the images
-					foreach ( $images as $image ) :
+					if ( $images ) :
+						foreach ( $images as $image ) :
 
-						// Get the image URL
-						$image_url = wp_get_attachment_image_url( $image, 'product' );
+							// Get the image URL
+							$image_url = wp_get_attachment_image_url( $image, 'product' );
 
-						// Output the image
-						// echo '<img class="background-image w-full h-[800px] bg-cover bg-center grow" src="' . $image_url . '">';
-						echo '<div class="background-image w-full bg-cover bg-center grow aspect-square" style="background-image: url(' . $image_url . ');"></div>';
+							// Output the image
+							// echo '<img class="background-image w-full h-[800px] bg-cover bg-center grow" src="' . $image_url . '">';
+							echo '<div class="background-image w-full bg-cover bg-center grow aspect-square" style="background-image: url(' . $image_url . ');"></div>';
 
-					endforeach;
+						endforeach;
+					endif;
 					?>
 				</div>
 				<?php if ( $images ) : ?>
--- template-parts/section-category.php
+++ template-parts/section-category.php
@@ -1,7 +1,7 @@
 <?php $full_height = $args['full-height'] ?? true; ?>
 
 <section id="section-category" class="<?php echo $full_height ? 'h-screen ' : ''; ?>!overflow-x-hidden bg-black flex flex-col justify-center">
-    <div class="relative flex flex-col lg:flex-row justify-center items-center lg:h-2/3 h-full self-center w-full pt-24 lg:pt-0">
+    <div class="relative flex flex-col lg:flex-row justify-center items-center lg:h-2/3 h-full self-center w-full md:pt-24 lg:pt-0">
         <?php
         $categories = array(
             array(
@@ -27,7 +27,7 @@
             <?php if (is_wp_error($category_link)) $category_link = '#'; ?>
             <div class="lg:w-[33.33333vw] w-full lg:h-[33.33333vw] h-1/2">
                 <a href="<?php echo esc_attr($category_link); ?>" class="h-full mx-auto 2xl:mt-0 2xl:translate-y-0 flex flex-col justify-center">
-                    <div class="card-zoom">
+                    <div class="card-zoom min-h-[150px]">
                         <div class="card-zoom-image bg-<?php echo $category['class']; ?>"></div>
                         <h1 class="card-zoom-text"><?php echo $category['title']; ?></h1>
                     </div>
