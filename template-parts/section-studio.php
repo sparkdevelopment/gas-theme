8df9292799b79609f6a60f0418d15c63395415d8
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-27 11:57:37 +0100

    Contact fixed

--- template-parts/section-studio.php
+++ template-parts/section-studio.php
@@ -5,8 +5,8 @@
     <div class="lg:w-[25vw] w-full lg:h-full h-[35vh] font-theme-heading text-black lg:mr-12 xl:mr-40 2xl:mr-40 xl:ml-4 lg:ml-2 lg:text-left text-center flex">
         <div class="m-auto text-left">
             <img src="<?php echo get_theme_file_uri(); ?>/resources/img/studios-logo.png" class="fade-in w-auto max-h-[8vh] 2xl:max-h-[120px] sm:-ml-1 2xl:-ml-2">
-            <p class="fade-in block text-[3.8vmin] lg:text-lg xl:text-xl 2xl:text-2xl font-light lg:py-2 xl:py-3 px-4">Everything you need. None of the faff. Three fully serviced North London studios with infinity coves, daylight options, great coffee and across the road from our kit warehouse.</p>
-            <a class="fade-in block text-[4vmin] lg:text-lg xl:text-xl 2xl:text-2xl font-normal px-4 my-2 lg:my-0" href="https://gasstudios.co.uk" target="_blank">Find out more <img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" class="-rotate-90 w-2 lg:w-5 inline ml-4"></a>
+            <p class="fade-in block text-[3.8vmin] md:text-regular lg:text-lg xl:text-xl 2xl:text-2xl font-light lg:py-2 xl:py-3 px-4">Everything you need. None of the faff. Three fully serviced North London studios with infinity coves, daylight options, great coffee and across the road from our kit warehouse.</p>
+            <a class="fade-in block text-[4vmin] md:text-regular lg:text-lg xl:text-xl 2xl:text-2xl font-normal px-4 my-2 lg:my-0" href="https://gasstudios.co.uk" target="_blank">Find out more <img src="<?php echo get_theme_file_uri(); ?>/resources/img/arrow-black.svg" class="-rotate-90 w-2 lg:w-5 inline ml-4"></a>
         </div>
     </div>
 </section>
