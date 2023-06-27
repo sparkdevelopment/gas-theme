c7549707e54d85c8a86090cc2644614893256aa2
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-06-27 11:07:19 +0100

    Fix brands/footer sections

--- template-parts/section-brands-and-instagram.php
+++ template-parts/section-brands-and-instagram.php
@@ -1,6 +1,6 @@
-<section id="section-brands-and-instagram" class="h-screen bg-black">
+<section id="section-brands-and-instagram" class="h-screen bg-black flex flex-col justify-between md:pt-24">
 
-    <div class="h-1/2 lg:h-1/3 relative">
+    <div class="h-1/2 lg:h-1/5 relative">
         <div class="owl-carousel h-full !flex flex-col justify-center item">
             <!-- Carousel items -->
             <?php
@@ -11,12 +11,12 @@
         </div>
     </div>
 
-    <div class="h-1/2 lg:h-1/3 relative">
+    <div class="h-1/2 lg:h-2/5 relative">
         <!-- <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-full items-center pt-12 px-4"> -->
-        <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-full items-center px-[4vw]">
+        <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-full items-center px-0">
             <!-- Instagram posts -->
             <?php for ($i = 1; $i <= 4; $i++) : ?>
-                <img src="<?php echo get_theme_file_uri(); ?>/resources/img/ig-posts/post<?php echo $i; ?>.jpeg" class="max-h-full w-auto basis-[40%] md:basis-[25%] mx-auto p-[1.5vw]">
+                <img src="<?php echo get_theme_file_uri(); ?>/resources/img/ig-posts/post<?php echo $i; ?>.jpeg" class="max-h-full w-auto basis-[40%] md:basis-[25%] mx-auto p-[1rem]">
             <?php endfor; ?>
         </div>
     </div>
--- template-parts/section-footer.php
+++ template-parts/section-footer.php
@@ -1,15 +1,15 @@
-<div class="bg-black h-screen md:h-1/3 flex flex-col justify-end">
-	<div class="sm:grid sm:grid-cols-12 flex flex-col md:flex-col-reverse justify-between gap-4 text-white text-base sm:text-md sm:mx-8 md:mx-0 font-light leading-6">
-		<div class="col-span-12 md:col-span-3 order-1 md:order-none">
+<div class="bg-black h-screen md:h-auto flex flex-col justify-between">
+	<div class="sm:grid grid-cols-5 xl:grid-cols-8 2xl:grid-cols-10 flex flex-col md:flex-col-reverse justify-between gap-4 text-white text-sm sm:text-md sm:mx-8 md:mx-0 font-light leading-5">
+		<div class="col-span-5 lg:col-span-1 order-1 sm:order-none">
 			<?php if ( has_custom_logo() ) : ?>
 			<div class="w-full">
-				<div class="pt-1 sm:pb-4 w-full">
+				<div class="pt-2 md:pb-4 w-full">
 					<div class="flex justify-between items-center">
 						<div class="h-24 md:h-auto js-header-logo xl:ml-4 sm:w-auto max-w-[75%] md:max-w-[35%] xl:max-w-[10%]">
-							<div class="js-footer-logo w-60 h-full items-center md:flex hidden">
+							<div class="js-footer-logo w-36 h-full items-center md:flex hidden">
 								<div><?php the_custom_logo(); ?></div>
 							</div>
-							<div class="js-footer-logo w-60 h-full md:hidden flex">
+							<div class="js-footer-logo w-36 h-full md:hidden flex">
 								<img src="<?php echo get_template_directory_uri(); ?>/resources/img/mobile-logo.svg">
 							</div>
 						</div>
@@ -18,12 +18,12 @@
 			</div>
 			<?php endif; ?>
 		</div>
-		<div class="flex flex-col col-span-5 md:col-span-2 mx-4 order-4 md:order-none">
-			<p class="sm:my-4 my-2">
+		<div class="flex flex-col col-span-5 sm:col-span-1 mx-4 order-4 sm:order-none">
+			<p class="sm:my-4 my-2 whitespace-nowrap">
 				<a href="mailto:office@gashire.co.uk?subject=Enquiry from website">office@gashire.co.uk</a><br>
 				<a href="tel:+442033972757">+44 (0)20 3397 2757</a>
 			</p>
-			<address class="sm:mb-6 sm:mt-0 my-2 not-italic">
+			<address class="sm:mb-6 sm:mt-0 my-2 not-italic leading-4 whitespace-nowrap">
 				GAS Production Hire<br>
 				Unit 8 A/B<br>
 				The High Cross Centre<br>
@@ -32,7 +32,7 @@
 			</address>
 			<p class="sm:my-4 my-2 pt-2 block sm:hidden">&copy; GAS Production Hire Ltd <?php echo date_i18n( 'Y' ); ?></p>
 		</div>
-		<div class="col-span-6 md:col-span-2 mx-4 order-3 md:order-none">
+		<div class="col-span-5 sm:col-span-1 mx-4 order-3 sm:order-none">
 			<ul class="sm:my-4 my-2">
 				<li><a href="/info-contact/">Info & Contact</a></li>
 				<li><a href="/careers/">Careers</a></li>
@@ -40,7 +40,7 @@
 				<li><a href="/cookie-policy/">Cookie Policy</a></li>
 			</ul>
 		</div>
-		<div class="md:col-start-9 md:col-end-13 col-span-12 md:col-span-4 flex flex-col justify-end mx-4 order-2 md:order-none">
+		<div class="col-start-3 lg:col-start-4 xl:col-start-6 2xl:col-start-8 col-end-6 lg:col-end7 xl:col-end-9 2xl:col-end-11 2xl:col-span-3 flex flex-col justify-end mx-4 order-2 sm:order-none text-base">
 			<?php
 			// Add widget area for footer newsletter form
 			if ( is_active_sidebar( 'footer-newsletter' ) ) {
