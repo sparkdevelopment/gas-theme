f71d55681c40592446bb0715f88f2bb747935aff
Ryan Jarrett <91252132+gmg-ryan@users.noreply.github.com>
2023-07-25 17:33:47 +0100

    Connect IG feed

--- template-parts/section-brands-and-instagram.php
+++ template-parts/section-brands-and-instagram.php
@@ -15,9 +15,17 @@
         <!-- <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-full items-center pt-12 px-4"> -->
         <div class="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-full items-center px-0">
             <!-- Instagram posts -->
-            <?php for ($i = 1; $i <= 4; $i++) : ?>
+            <!--<?php for ($i = 1; $i <= 4; $i++) : ?>
                 <img src="<?php echo get_theme_file_uri(); ?>/resources/img/ig-posts/post<?php echo $i; ?>.jpeg" class="max-h-full w-auto basis-[40%] md:basis-[25%] mx-auto p-[1rem]">
-            <?php endfor; ?>
+            <?php endfor; ?>-->
+            <?php
+            // Get instagram posts from remote JSON feed
+            $json = file_get_contents('https://feeds.behold.so/mxyI1TJgu5ev8zj5TA0K');
+            $posts = json_decode($json);
+            foreach ($posts as $post) : ?>
+                <?php $thumb_url = $post->mediaType == 'IMAGE' ? $post->mediaUrl : $post->thumbnailUrl; ?>
+                    <img src="<?php echo $thumb_url; ?>" class="max-h-full w-auto basis-[40%] md:basis-[25%] mx-auto p-[1rem] object-cover aspect-square">
+            <?php endforeach; ?>
         </div>
     </div>
 
